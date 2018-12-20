bf2cc3b5bae9be819b68075cfaa0854c2774ea6a
Kanstantsin <kanstantsin.litvin@gmail.com>
2018-12-20 23:07:57 +0300

    first commit

---
+++ js/cart.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function handleCartClick(event){
+  if (event.target.classList.contains('add-to-cart')){
+    event.preventDefault();
+    let title = event.target.dataset.title;
+    let price = event.target.dataset.price;
+    addToCart({title, price});
+  }
+}
+
+const itemsList = document.querySelector('.items-list');
+itemsList.addEventListener('click', handleCartClick);
